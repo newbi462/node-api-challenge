2f485db781dad7c648cc42e18a52077b0d9401c8
newbi462 <isthisemailfree@gmail.com>
2020-01-17 11:26:01 -0500

    nodemon dev dependancy crap

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -3,3 +3,7 @@ const knexConfig = require("../knexfile.js");
 const environment = process.env.NODE_ENV || "development";
 
 module.exports = knex(knexConfig[environment]);
+
+
+//dose "Make nodemon be a development time dependency only, it shouldn't be deployed to production." mean chcnge the NODE part of this file?
+//This is wehy I asked, because our only instrution is WE WILL COVER THIS NEXT WEEK... 
