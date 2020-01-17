f4a1d7308115259689cdbbcc864f1b130c657499
newbi462 <isthisemailfree@gmail.com>
2020-01-17 11:56:41 -0500

    MVP SUDO SET UP

---
+++ routs/projectModelRouter.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const ProjectModel = require("./../data/helpers/projectModel.js");
+
+const router = express.Router();
+
+//CRUD
+
+//GET
+
+//POST
+
+//PUT
+
+//DEL
+
+//SOME BY ID
+
+
+
+
+//When adding an action, make sure the project_id provided belongs to an existing project. If you try to add an action with an id of 3 and there is no project with that id the database will return an error.
+
+module.exports = router;
